8124d1b7d2d0ffb0ef610fd737748ebf7edb9a18
eliemaze <elie.maze@gmail.com>
2021-01-08 18:17:29 +0100

    settings

--- disquaire_project/settings.py
+++ disquaire_project/settings.py
@@ -149,7 +149,7 @@ INTERNAL_IPS = ['127.0.0.1']
 
 
 if os.environ.get('ENV') == 'PRODUCTION':
-    PROJECT_ROOT = os.path.join.dirname(os.path.abspath(__file__))
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
     STATICFILES_DIRS = ( os.path.join(PROJECT_ROOT, 'static'), )
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
